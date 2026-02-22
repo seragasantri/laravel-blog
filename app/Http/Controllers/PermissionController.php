f45c0ff396fac87179bbcc161289073637e4f388
seragasantri <seragasantri@gmail.com>
2026-02-22 16:04:45 +0700

    feat(permission): implementasi crud dasar pada modul permission

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Permissions\StoreRequest;
+use App\Http\Requests\Permissions\UpdateRequest;
+use Inertia\Inertia;
+use Spatie\Permission\Models\Permission;
+
+class PermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Inertia::render('permissions/index', [
+            'permissions' => Permission::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('permissions/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request)
+    {
+        Permission::create($request->validated());
+        return to_route('permissions.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Permission $permission)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Permission $permission)
+    {
+        return Inertia::render('permissions/edit', [
+            'permission' => $permission
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, Permission $permission)
+    {
+        $permission->update($request->validated());
+        return to_route('permissions.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Permission $permission)
+    {
+        $permission->delete();
+        return to_route('permissions.index');
+    }
+}
---
+++ resources/js/pages/permissions/table/column.tsx
@@ -0,0 +1,54 @@
+import { destroy, edit } from "@/routes/permissions";
+import { Permission } from "@/types";
+import { Link } from "@inertiajs/react";
+import { ColumnDef } from "@tanstack/react-table";
+import { Edit, Trash } from "lucide-react";
+
+export const PermissionColumn: ColumnDef<Permission>[] = [
+    {
+        id: "index",
+        header: "#",
+        meta: { className: "text-center" },
+        cell: ({ row }) => {
+            return <div className="text-center">{row.index + 1}</div>;
+        },
+        enableSorting: false,
+    },
+    {
+        accessorKey: "name",
+        header: "Name",
+
+    },
+    {
+        accessorKey: "permissions",
+        header: "Permissions",
+        enableSorting: false,
+        cell: ({ row }) => {
+            const permissions = row.original.permissions || [];
+            return (
+                <div className="flex flex-wrap gap-1">
+                    {permissions.map((permission) => (
+                        <span key={permission.id} className="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded-full">
+                            {permission.name}
+                        </span>
+                    ))}
+                </div>
+            );
+        },
+    }, {
+        // action
+        id: "actions",
+        header: "Actions",
+        cell: ({ row }) => {
+            const user = row.original;
+
+            return (
+                <div className="flex gap-2">
+                    <Link href={edit(user)} className="p-3 bg-yellow-500 text-white rounded-full"><Edit className="size-4" /></Link>
+                    <Link href={destroy(user)} className="p-3 bg-red-500 text-white rounded-full"><Trash className="size-4" /></Link>
+
+                </div>
+            );
+        }
+    }
+]
